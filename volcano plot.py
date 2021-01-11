8aed33ffd84b49e40f110c6877b23fda34656a7f
morgancy711 <77252106+morgancy711@users.noreply.github.com>
2021-01-11 13:24:43 +1300

    Update volcano plot.py

--- volcano plot.py
+++ volcano plot.py
@@ -2,4 +2,5 @@ import pandas as pd
 from bioinfokit import visuz
 df = pd.read_csv("CD11b diff ex. secreted genes vs PBS.csv")
 df.head()
-visuz.gene_exp.volcano(d=df, lfc= "L2FC", pv="p-value", color=('#E10600FF','#00239CFF'), gstyle=2,  sign_line=True)
+visuz.gene_exp.volcano(d=df, lfc= "L2FC", pv="p-value", color=('#E10600FF','#00239CFF'), gstyle=2
+                       ,  sign_line=True, xlm=(-6,6,1), ylm=(0,120,20), figtype='svg')
