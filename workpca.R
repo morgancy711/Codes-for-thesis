a1724a00fa8078a0b924bee6af1dc7e4e7490c66
morgancy711 <77252106+morgancy711@users.noreply.github.com>
2021-05-03 22:17:30 +1200

    Update workpca.R

--- workpca.R
+++ workpca.R
@@ -2,14 +2,17 @@ library(biomaRt)
 library(DESeq2)
 library(magrittr)
 library(tidyverse)
+library(ggfortify)
 
-raw_counts <- read.csv("/Users/cynth/rnaseq.csv",
+VSTpk <- read.csv("/Users/cynth/OneDrive/Documents/rnaseq1.csv",
                        row.names = 1,
-                       header = TRUE)
-metadata <- read.csv("/Users/cynth/metadata.csv",
+                       header = TRUE,
+                       )
+
+metadata <- read.csv("/Users/cynth/OneDrive/Documents/metadata.csv",
                      row.names = 1,
                      header = TRUE)
-dds <- DESeqDataSetFromMatrix(countData = raw_counts,
+dds <- DESeqDataSetFromMatrix(countData = VSTpk,
                               colData = metadata,
                               design = ~ Sample) %>%
   DESeq()
@@ -17,8 +20,18 @@ dds <- DESeqDataSetFromMatrix(countData = raw_counts,
 dds <- vst(dds)
 rld <- rlog(dds)
 
+gene.exclude <- as.numeric(which(apply(t(VSTpk), 2, var)==0))
 
+pca_res <- prcomp(t(VSTpk)[,-gene.exclude], scale. = TRUE)
+autoplot(pca_res)
 
-  print( plotPCA(rld, intgroup = c( "Cell", "Antigen")) )
-
-       
+ggplot(dat = pca_res$x, aes(x = PC1, y = PC2)) + 
+  geom_point(aes(colour = colouring), size = 3)+
+  theme_bw(
+  )+xlab(paste("PC1:", round(pca_res$sdev[1]^2/sum(pca_res$sdev^2)*100, 3), "%")
+         ) +ylab(paste("PC2:", round(pca_res$sdev[2]^2/sum(pca_res$sdev^2)*100, 
+                                     3), "%")
+         )
+colouring <- c(rep("CD11b_pos", 3), rep("CD11b_neg", 3),
+               rep("TN_pos", 3), rep("TN_neg", 3))
+               
